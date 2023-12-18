5b06ab06e2b3769a15312e907b44bf46dcc2a123
Niel Thiart <niel@thiart.com>
2023-12-18 14:01:41 +0200

    Comments

--- shots.sh
+++ shots.sh
@@ -1,20 +1,22 @@
 # Usage: ./shots.sh
 # Description: Generate screenshots of the examples
+# Pages tested:
+# - https://www.bellingcat.com/resources/2016/12/06/vacuuming-image-metadata-wayback-machine/
 
-# Default
+# Default Bellingcat
 shot-scraper "https://www.bellingcat.com/resources/2016/12/06/vacuuming-image-metadata-wayback-machine/" -w 1200 -h 800 \
   -o examples/1-bellingcat-default.png
 
-# No font hinting
+# No font hinting Bellingcat
 shot-scraper "https://www.bellingcat.com/resources/2016/12/06/vacuuming-image-metadata-wayback-machine/" -w 1200 -h 800 \
   -o examples/2-bellingcat-font-render-hinting-none.png \
   --browser-args "--font-render-hinting=none"
 
-# Default with geometricprecision
+# Default with geometricprecision Bellingcat
 shot-scraper "https://www.bellingcat.com/resources/2016/12/06/vacuuming-image-metadata-wayback-machine/" -w 1200 -h 800 \
   -o examples/3-bellingcat-default-geomp.png --javascript "(document.head || document.documentElement).appendChild(Object.assign(document.createElement('style'), {textContent: '*, ::before, ::after { text-rendering: geometricPrecision !important; }'}));"
 
-# No font hinting with geometricprecision
+# No font hinting with geometricprecision Bellingcat
 shot-scraper "https://www.bellingcat.com/resources/2016/12/06/vacuuming-image-metadata-wayback-machine/" -w 1200 -h 800 \
   -o examples/4-bellingcat-font-render-hinting-none-geomp.png \
   --browser-args "--font-render-hinting=none" \
