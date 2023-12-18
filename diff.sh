3f76b00b755df39d2b41aba5d5ad5935cc0b9201
Niel Thiart <niel@thiart.com>
2023-12-18 13:17:51 +0200

    test

--- diff.sh
+++ diff.sh
@@ -3,8 +3,8 @@
 # examples/3-bellingcat-default-geomp.png
 # examples/4-bellingcat-font-render-hinting-none-geomp.png
 
-image-diff --output diffs/diff-1-2.png examples/1-bellingcat-default.png examples/2-bellingcat-font-render-hinting-none.png
+image-diff examples/1-bellingcat-default.png examples/2-bellingcat-font-render-hinting-none.png --output diffs/diff-1-2.png 
 
-image-diff --output diffs/diff-1-3.png examples/1-bellingcat-default.png examples/3-bellingcat-default-geomp.png
+image-diff examples/1-bellingcat-default.png examples/3-bellingcat-default-geomp.png --output diffs/diff-1-3.png
 
-image-diff --output diffs/diff-1-4.png examples/1-bellingcat-default.png examples/4-bellingcat-font-render-hinting-none-geomp.png
+image-diff examples/1-bellingcat-default.png examples/4-bellingcat-font-render-hinting-none-geomp.png --output diffs/diff-1-4.png
